99d472e28df456ccbe9b38481959af58ad993b51
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2025-06-17 01:26:41 -0300

    FEAT: Fiz o sistema de zipline e junto com isso fiz o sargento.

--- Assets/Scripts/Soldiers/Characters/Sargeant.cs
+++ Assets/Scripts/Soldiers/Characters/Sargeant.cs
@@ -1,16 +1,21 @@
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Sargeant : ISoldierState
-{
-    public override void OnEnter(SoldierManager soldierManager) {
-        base.OnEnter(soldierManager);
-		Debug.Log("Sargeant state entered.");
+public class Sargeant : ISoldierState {
+    [SerializeField] private float checkOffSet = 1f;
+    [SerializeField] private float checkRadius = 2f;
+
+    [SerializeField] private GameObject _originalParent;
+
+    public override void OnUpdate() {
+        if (Input.GetKeyDown(KeyCode.E)) {
+            RaycastHit[] hits = Physics.SphereCastAll(transform.position + new Vector3(0, checkOffSet, 0), checkRadius, Vector3.up);
+
+            foreach(RaycastHit hit in hits) {
+                if (hit.collider.tag == "Zipline") {
+                    hit.collider.GetComponent<Zipline>().StartZipLine(this.gameObject);
+                }
+            }
+        }
     }
-    public override void OnUpdate()
-	{
-	}
-	public override void OnExit()
-	{
-        base.OnExit();
-	}
+    public Transform ResetParentToOriginal() => this.gameObject.transform.parent = _originalParent.transform;
 }
