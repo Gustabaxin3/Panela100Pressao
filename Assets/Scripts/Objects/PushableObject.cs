8ebbaac303d25476db3da85d2e3e9d9d6334e0ec
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2025-06-18 22:53:23 -0300

    FEAT: Fiz o script de empurrar e arrastar objetos.

--- Assets/Scripts/Objects/PushableObject.cs
+++ Assets/Scripts/Objects/PushableObject.cs
@@ -1,26 +1,49 @@
-using System.Collections;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class PushableObject : MonoBehaviour {
     private Rigidbody _rigidBody;
-    private void Awake() {
-        if(_rigidBody == null) this.AddComponent<Rigidbody>();
+    private Transform _pusher;
+    private Vector3 _offset;
 
+    public bool IsBeingPushed => _pusher != null;
+
+    private void Awake() {
         _rigidBody = GetComponent<Rigidbody>();
         _rigidBody.constraints = RigidbodyConstraints.FreezeAll;
     }
-    public void Push(Vector3 direction, float force) {
+
+    public void StartPush(Transform pusher) {
+        InteractionHintUI.Instance.HideHint();
+
+        _pusher = pusher;
+        _offset = transform.position - _pusher.position;
+
         _rigidBody.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionY;
-
-        _rigidBody.AddForce(direction.normalized * force, ForceMode.Impulse);
-
-        StartCoroutine(FreezeLater());
     }
-    private IEnumerator FreezeLater() {
-        yield return new WaitForSeconds(0.5f);
+
+    public void StopPush() {
+        _pusher = null;
         _rigidBody.linearVelocity = Vector3.zero;
         _rigidBody.constraints = RigidbodyConstraints.FreezeAll;
     }
+
+    private void FixedUpdate() {
+        if (_pusher != null) {
+            Vector3 targetPosition = _pusher.position + _offset;
+            Vector3 moveDirection = (targetPosition - transform.position);
+            _rigidBody.linearVelocity = moveDirection * 10f;
+            InteractionHintUI.Instance.ShowHint("Pressione E para parar de empurrar.");
+        }
+    }
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Sublieutenant")) {
+            InteractionHintUI.Instance.ShowHint("Pressione E para empurrar.");
+        }
+    }
+    private void OnCollisionExit(Collision collision) {
+        if (collision.gameObject.CompareTag("Sublieutenant")) {
+            InteractionHintUI.Instance.HideHint();
+        }
+    }
 }
+
